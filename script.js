079b8b5400ffda5e8ee3fa1f7a3d75fbf2bc304b
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2018-08-09 19:14:41 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+class Component
+{
+    notify() {
+        this.callback();
+    }
+
+    register(callback) {
+        this.callback = callback;
+    }
+
+    render() {}
+}
+
+class Renderer
+{
+    constructor(component, destination) {
+        this.render = component.render.bind(component);
+        this.destination = destination;
+
+        component.register(() => {
+            return this.listen();
+        });
+
+        this.listen();
+    }
+
+    listen () {
+        this.destination.innerHTML = '';
+        this.destination.appendChild(this.render());
+    }
+}
+
