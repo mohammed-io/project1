1bd6d55c185a7ff8fdc7afbef77df410589c68c6
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2018-08-12 16:14:12 +0300

    The stopwatch

--- script.js
+++ script.js
@@ -30,28 +30,47 @@ class Renderer
     }
 }
 
-
-class Counter extends Component
+class Stopwatch extends Component
 {
     constructor() {
         super();
+        
+        this.time = 0;
+    }
 
-        this.count = 0;
+    start() {
+        this.handle = setInterval(() => {
+            this.time += 10;
+
+            this.notify();
+        }, 10);
+    }
+
+    pause() {
+        clearInterval(this.handle);
+    }
+
+    formatTime(number) {
+        const hours = Math.floor(number / 1000 / 60 / 60);
+        const minutes = Math.floor(number / 1000 / 60) % (3600);
+        const seconds = Math.floor(number / 1000) % 60;
+        const milliSeconds = number % 1000;
+
+        return `${('0' + hours).slice(-2)}:${('0' + minutes).slice(-2)}:${('0' + seconds).slice(-2)}.${('00' + milliSeconds).slice(-3)}`;
+    }
+
+    reset() {
+        this.time = 0;
+        this.notify();
     }
 
     render() {
-        return $('<div>')
-            .append($('<p>')
-            .html(`Count: ${this.count}`)
-            ).append([
-                $('<button>').html('Increment').on('click', () => {
-                    this.count++;
-                    this.notify();
-                }),
-                $('<button>').html('Decrement').on('click', () => {
-                    this.count--;
-                    this.notify();
-                })
-            ])[0];
+        return $('<div class="col-md-12 text-center mono-font">')
+        .append([
+            $('<h1>').html(this.formatTime(this.time)),
+            $('<button class="btn btn-primary">').html('Start').on('click', this.start.bind(this)),
+            $('<button class="btn btn-warning">').html('Pause').on('mousedown', this.pause.bind(this)),
+            $('<button class="btn btn-danger">').html('Reset').on('click', this.reset.bind(this))
+        ])[0];
     }
 }
